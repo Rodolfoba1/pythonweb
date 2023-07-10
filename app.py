7680a2ff3e639491148a9e40ada69653ce367a34
Rodolfoba1 <38680428+Rodolfoba1@users.noreply.github.com>
2023-07-10 01:31:14 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+
+app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://usuario:Pass123*@3.145.80.6:3306/bd'
+db = SQLAlchemy(app)
+
+# Definir un modelo para tabla
+class Compras(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(100))
+    cantidad = db.Column(db.String(100))
+
+@app.route('/')
+def mostrar_datos():
+    # Consultar los datos de la base de datos y ordenar por la columna id
+    compras = Compras.query.order_by(Compras.id).all()
+    return render_template('index.html', compras=compras)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
